7b6e68655c9b3c5601df699f003203a4159d3ab9
amnakifayat13 <hayatullahamna@gmail.com>
2024-03-21 20:25:41 +0500

    first push by command prompt

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+// Asking Questions from Users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+//Conditional statements if-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+    
+}
+
+else if(answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+    
+}
+
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+    
+}
+
+else if(answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+    
+}
+else{
+    console.log("Invalid Input")
+    
+}
